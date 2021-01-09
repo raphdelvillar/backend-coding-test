4510bb8d2c59730e093a246aad6481f661981124
raphdelvillar <raphdelvillar@gmail.com>
2021-01-10 02:41:22 +0800

    add esdocs and swagger

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = 8010;
-
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
+const swaggerUi = require('swagger-ui-express');
+
 const buildSchemas = require('./src/schemas');
+const buildSwagger = require('./src/swagger');
 
 db.serialize(() => {
+
     buildSchemas(db);
 
-    const app = require('./src/app')(db);
+    const routes = require('./src/routes')(db);
+    buildSwagger(port,routes);
+
+    const app = require('./src/app')(routes);
+
+    app.use(express.static('docs'))
+
+    const swaggerDocument = require('./swagger.json');
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    app.get('/', (_, res) => {
+        res.sendFile(path.join(__dirname + '/docs/index.html'));
+    })
 
     app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
